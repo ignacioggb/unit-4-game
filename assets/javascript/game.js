f280852c75bb83dce98f860eb5b27333d1a70502
Ignacio Gaxiola <ignacioggb@hotmail.com>
2019-03-10 22:37:42 -0700

    first part

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+
+
+/*
+
+$(".letter-button").on("click", function () {
+    var fridgeMagnet = $("<div>");
+    fridgeMagnet.attr("class", "card");
+    fridgeMagnet.text($(this).attr("data-letter"));
+    $("#display").append(fridgeMagnet);
+    //captainPlanet.animate({ opacity: "0.05" });
+    var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+    img.attr('src', responseObject.imgurl);
+    img.appendTo('#imagediv');
+    $("#clear").on("click", function () {
+        $("#display").empty();
+      })
+
+  })
+
+*/
